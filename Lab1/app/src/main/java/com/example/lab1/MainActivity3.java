b06b0d7983d11e0fe514fad6c7550f0be4cdca3d
Ksenia Maltseva <ksenya.maltseva1901@inbox.ru>
2024-03-05 16:06:41 +0900

    1App

---
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity3.java
@@ -0,0 +1,15 @@
+package com.example.lab1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lab1.R;
+
+public class MainActivity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
